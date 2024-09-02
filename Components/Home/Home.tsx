5b56f3d1c060b698284898e36cdc0fab931160d9
Kartik Hatwar <kartikhatwar98@gmail.com>
2024-09-02 23:47:39 +0530

    title centred

--- Components/Home/Home.tsx
+++ Components/Home/Home.tsx
@@ -3,7 +3,7 @@ import { firstName } from "@/INFO";
 const Home = ()=>{
     return <div id="home" className="min-w-full HomeDiv min-h-[100vh]">
         <div className="overlay min-w-full min-h-full flex justify-center items-center">
-                <div className="Title  text-white flex flex-col gap-4">
+                <div className="Title  text-white flex flex-col gap-4 items-center justify-center">
                     <div className="Name text-[3.75rem] tracking-[3px] font-light">HI, I&apos;M {firstName?.toLocaleUpperCase() || "User"}</div>
                     <div className="Position text-[#ddd] text-center ">
                         Software Developer & Web Enthusiast
