ad39de9631bd8f8da4faa700c70c05c232c81252
kartik <kartikhatwar98@gmail.com>
2024-09-28 16:57:24 +0530

    experience done

--- Components/Experience/Experience.tsx
+++ Components/Experience/Experience.tsx
@@ -1,29 +1,23 @@
-import { experience, expTagline, skills, aboutme } from "@/INFO";
+import { experience, expTagline, aboutme } from "@/INFO";
 import { Lato, Roboto_Slab } from "next/font/google";
 import { Tooltip } from "react-tooltip";
 import Image from "next/image";
-import { useScroll, useTransform , motion } from "framer-motion";
-import { useRef } from "react";
+import {  motion } from "framer-motion";
 
 const lato = Lato({ weight: "300", subsets: ["latin"] });
 const robotSlab = Roboto_Slab({ weight: "300", subsets: ["latin"] });
 
 const Experience = () => {
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({target: ref,offset: ["start start", "end start"],});
-  const translateX1 = useTransform(scrollYProgress,[0, 0.5,1],[0, 1000 ,1000],{ clamp: false })
-  
-
   
   return (
-    <div ref={ref} id="" className="flex flex-col items-center bg-slate-900 text-slate-400 ">
-      <div className="overlay absolute h-full w-full" />
+    <div id="" className="flex flex-col min-h-[100vh] items-center bg-slate-900 text-slate-400 ">
+      {/* <div className="overlay absolute h-full w-full" /> */}
 
-      <div className={`w-full min-h-[100vh] px-6 gap-4 py-24 border-[4px] border-[green] relative flex z-[1] justify-start items-start`}>
+      <div className={`w-full min-h-[100vh] px-6 gap-4 py-24 relative flex z-[1] justify-start items-start`}>
 
-            <div className="left basis-1/2 sticky top-36 border flex flex-col justify-center">
+            <div className="left basis-1/2 sticky top-36 flex flex-col justify-center">
 
-              <div className={`text-center border items-center justify-center flex mb-16 font-bold tracking-[1px] text-3xl`}>
+              <div className={`text-center items-center justify-center flex mb-16 font-bold tracking-[1px] text-3xl`}>
                 Experience
               </div>
               <div style={robotSlab.style} className="expTagline w-full text-lg font-light leading-[1.7] text-center">
@@ -31,12 +25,12 @@ const Experience = () => {
               </div>
             </div>
 
-            <div className="right basis-1/2 flex flex-col border justify-center items-center">
+            <div className="right basis-1/2 flex flex-col justify-center items-center">
 
-              <div className=" flex flex-col gap-[120vh] justify-evenly items-center">
+              <div className=" flex flex-col gap-16 justify-evenly items-center">
                 {experience?.map((item, i) => {
                   return (
-                    <motion.div key={`pillExp_${i}`} style={{translateX : i == 0 ? translateX1 : 0}} className="pillofComp border sticky top-36 w-[70%] flex flex-col gap-2 rounded-lg p-3">
+                    <motion.div key={`pillExp_${i}`}  className="pillofComp cursor-pointer border-slate-800 hover:border-transparent hover:border-t-slate-700 -inset-x-4 -inset-y-4 z-0 transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg hover:bg-slate-800 duration-300 border w-[70%] flex flex-col gap-2 rounded-xl p-5">
                       <div className="compName text-center text-2xl">
                         {item.companyName}
                       </div>
@@ -48,6 +42,7 @@ const Experience = () => {
                         <div style={lato.style} className="basis-auto">
                           {item.location}
                         </div>
+                        -
                         <div style={lato.style} className="basis-auto">
                           ({item.dates})
                         </div>
@@ -58,7 +53,7 @@ const Experience = () => {
                           return (
                             <span
                               key={`compSkill_${i}_${j}`}
-                              className="py-2 px-4 border rounded bg-[#313131] border-t-[#4f4f4f] border-b-[transparent] border-x-[#3d3d3d] cursor-default duration-1000"
+                              className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 "
                             >
                               {compSkill}
                             </span>
@@ -66,7 +61,7 @@ const Experience = () => {
                         })}
                       </div>
 
-                      <div style={lato.style} className="desc text-center text-md">
+                      <div style={lato.style} className="desc text-start mt-4 text-md">
                         {item.workInfo}
                       </div>
 
